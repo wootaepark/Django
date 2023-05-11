c75ba941db850c21d610b9524fc9914daa3f5140
박태우 <pakdal99@gmail.com>
2023-05-11 19:25:03 +0900

    django course 26 commit

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView
+from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView, AccountDeleteView
 
 app_name = "accountapp"
 # 주소 접근을 쉽게 하기 위한 app_name 불필요하게 중복 해주는 것이 아니다. 아래처럼 나중에 활용 가능
@@ -19,6 +19,7 @@ urlpatterns = [
     path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'),
     #pk 라는 이름의 int 정보를 받겠다 라는 의미
     path('update/<int:pk>', AccountUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>', AccountDeleteView.as_view(), name='delete'),
 
 
 ]
