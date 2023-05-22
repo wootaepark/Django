af621d4ef2b71eefc5504ee5c50cb2bb5e12385b
박태우 <pakdal99@gmail.com>
2023-05-23 01:30:02 +0900

    django course 33 commit

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, UpdateView
 
@@ -14,7 +14,6 @@ class ProfileCreateView(CreateView):
     model=Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
     template_name='profileapp/create.html'
 
     def form_valid(self, form): #form은 우리가 보는 form (forms.py에 있는 Form 클래스) 데이터
@@ -23,14 +22,20 @@ class ProfileCreateView(CreateView):
         temp_profile.save() # 그리고 최종적으로 저장
         return super().form_valid(form) # 나머지는 위의 ProfileCreateView 안에 있는 원래 결과를 리턴
 
+    def get_success_url(self):
+        return reverse('accountapp:detail',kwargs={'pk':self.object.user.pk})
+    # self에서 object가 가리키는 것은 위에서 지정해준 Profile이다. 거기에 연결된 user의 pk를 찾아서 넘겨주게 되는 것이다.
+
 @method_decorator(profile_ownership_required, 'get')
 @method_decorator(profile_ownership_required, 'post')
 class ProfileUpdateView(UpdateView):
     model=Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
     template_name='profileapp/update.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})
 
 
 
