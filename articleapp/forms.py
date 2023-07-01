22ee0f33b2bf189c82c8689ca5cbcc29b740e200
박태우 <pakdal99@gmail.com>
2023-07-01 22:05:37 +0900

    django course 44 commit

--- articleapp/forms.py
+++ articleapp/forms.py
@@ -1,11 +1,19 @@
 from django.forms import ModelForm
+from django import forms # forms를 가져옴, 대문자는 잘가져 오는데 소문자는 잘 가져오지 못하는 경향이 있다.
 
 from articleapp.models import Article
+from projectapp.models import Project
 
 
 class ArticleCreationForm(ModelForm):
+
+    content = forms.CharField(widget = forms.Textarea(attrs={'class':'editable text-left',
+                                                             'style': 'height : auto;'}))
+
+
+    project = forms.ModelChoiceField(queryset=Project.objects.all(), required=False)
     class Meta:
-        model=Article
+        model = Article
         fields=['title','image','project','content']
 
 
